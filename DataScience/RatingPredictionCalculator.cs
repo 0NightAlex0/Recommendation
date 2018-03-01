04dc9cc32bc0148ee1416a8d6281a2d3820db040
Alex <0887185@hr.nl>
2018-03-01 20:56:29 +0100

    first github desktop commit

---
+++ DataScience/RatingPredictionCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataScience
+{
+    class RatingPredictionCalculator
+    {
+
+        public Dictionary<int, double> Calculate(List<KeyValuePair<int, UserPreferance>> neighbours, List<int> articleIds)
+        {
+            Dictionary<int, double> ratingPredictions = new Dictionary<int, double>();
+            foreach (int articleId in articleIds)
+            {
+                double weightedRating = 0;
+                double sumSimilarties = 0;
+                foreach (KeyValuePair<int, UserPreferance> neighbour in neighbours)
+                {
+                    Dictionary<int, double> ratings = neighbour.Value.UserRatings;
+                    if (ratings.ContainsKey(articleId))
+                    {
+                        double similarity = neighbour.Value.similarity;
+                        weightedRating += ratings[articleId] * similarity;
+                        sumSimilarties += similarity;
+                    }
+                }
+                ratingPredictions.Add(articleId, weightedRating / sumSimilarties);
+
+            }
+            return ratingPredictions;
+        }
+    }
+}
---
+++ DataScience/UserPreferance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataScience
+{
+    class UserPreferance
+    {
+        Dictionary<int, double> _userRatings = new Dictionary<int, double>();
+        // temp similarity here
+        public double similarity;
+        public UserPreferance()
+        {
+        }
+        public Dictionary<int, double> UserRatings
+        {
+            get
+            {
+                return _userRatings;
+            }
+
+        }
+    }
+}

c3fa0d3b0d384d7f93971b55c79ea4eb3c0f21f5
Alex <0887185@hr.nl>
2018-03-02 19:19:58 +0100

    opdr1 done

--- DataScience/RatingPredictionCalculator.cs
+++ DataScience/RatingPredictionCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System;
 namespace DataScience
 {
     class RatingPredictionCalculator
@@ -30,6 +31,7 @@ namespace DataScience
             List<KeyValuePair<int, double>> ratingPredictions = new List<KeyValuePair<int, double>>();
             // which key does the target not contain
             List<int> missingKeys = new List<int>();
+
             foreach (KeyValuePair<int, UserPreferance> neighbour in neighbours)
             {
                 foreach (int ratingId in neighbour.Value.UserRatings.Keys)
@@ -46,20 +48,21 @@ namespace DataScience
                 int count = 0;
                 foreach (KeyValuePair<int, UserPreferance> neighbour in neighbours)
                 {
-                    if (count == 3)
-                    {
-                        // add to predictionList
-                        double rating = this.CalculateRating(neighbours, key);
-                        ratingPredictions.Add(new KeyValuePair<int, double>(key, rating));
-                        break;
-                    }
                     if (neighbour.Value.UserRatings.ContainsKey(key))
                     {
                         count++;
                     }
+                    if (count >= 3)
+                    {
+                        // add to predictionList
+                        double rating = CalculateRating(neighbours, key);
+                        ratingPredictions.Add(new KeyValuePair<int, double>(key, rating));
+                        break;
+                    }
+
                 }
             }
-
+            
             return ratingPredictions.OrderByDescending(pair => pair.Value).ToList();
         }
 
@@ -68,7 +71,7 @@ namespace DataScience
             List<KeyValuePair<int, double>> ratingPredictions = new List<KeyValuePair<int, double>>();
             foreach (int articleId in articleIds)
             {
-                double rating = this.CalculateRating(neighbours, articleId);
+                double rating = CalculateRating(neighbours, articleId);
                 ratingPredictions.Add(new KeyValuePair<int, double>(articleId, rating));
 
             }
